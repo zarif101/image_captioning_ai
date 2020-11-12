b9c8cda547f4b1a7dd95df2b3f19fa14bee85679
zarif101 <zarif.azher@gmail.com>
2020-11-11 16:17:27 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from utils import caption_path,caption_img
+import cv2
+from PIL import Image
+
+def main():
+    mode = st.sidebar.selectbox(
+    "Select mode",
+    ("Example", "Try it!",)
+)
+    st.title('Welcome to Capper! 😎')
+    st.header('AI Powered Automated Image Captioning')
+    st.write('Note: I cannot ensure the accuracy of the captions, and this is very much a work in progress')
+    if mode == 'Example':
+
+        st.header('Example caption')
+        example_img = cv2.imread('static/dog.jpg')
+        example_img = cv2.cvtColor(example_img,cv2.COLOR_BGR2RGB)
+        example_img = cv2.resize(example_img,(224,224))
+        st.image(example_img)
+        st.header(caption_path('static/dog.jpg'))
+    else:
+        uploaded_image = st.file_uploader("Upload an image!",type=['png','jpg','jpeg'])
+        st.write('If it is taking a long time, then your image may be too diffcult - please reload the page and try again with another picture..sorry')
+        if uploaded_image:
+            pic = Image.open(uploaded_image)
+            pic = np.array(pic)
+            pic = cv2.resize(pic,(224,224))
+            st.image(pic)
+            st.header(caption_img(pic))
+            st.write('To try another picture, select a different photo!')
+if __name__=='__main__':
+    main()
---
+++ temp.py
@@ -0,0 +1,58 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import cv2
+import numpy as np
+import pandas as pd
+import os
+import tensorflow as tf
+import tensorflow.keras as keras
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import load_model
+import pickle
+from tensorflow.keras.models import Model
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.applications.vgg16 import preprocess_input
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
+config = tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+feat_model = keras.applications.vgg16.VGG16(weights='imagenet')
+feat_model = Model(feat_model.input,feat_model.layers[-2].output)
+model = load_model('new_deal_v5.h5')
+tokenizer = pickle.load(open( "tokenizer_v1.pickle", "rb" ))
+
+def get_word(pred):
+    for e,w in tokenizer.word_index.items():
+        if w == pred:
+            return e
+
+def caption(img_path):
+
+    global feat_model,model,tokenizer
+
+    test_pic = cv2.imread(img_path)
+    test_pic = cv2.resize(test_pic,(224,224))
+    test_pic = test_pic.reshape((1,224,224,3))
+    test_pic = preprocess_input(test_pic)
+
+    test_pic = feat_model(test_pic)
+    test_pic = np.reshape(test_pic,(4096,))
+    test_pic = test_pic.reshape((1,-1))
+    words = 'startseq'
+    while 'endseq' not in words:
+        test_seq = [words]
+        test_seq = tokenizer.texts_to_sequences(test_seq)
+        test_seq = np.array(test_seq)
+        test_seq = pad_sequences(test_seq,maxlen=10)[0]
+        test_seq = test_seq.reshape((1,10))
+
+        pred = model([test_pic,test_seq])
+
+        pred = get_word(np.argmax(pred))
+        words = words+' '+pred
+    words = words.replace('startseq','')
+    words = words.replace('endseq','')
+    return words.rstrip().lstrip()
